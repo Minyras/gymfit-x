c2d4b7557c69f9c7480058ce5e4475ca4c74917f
Seyidova Minure <minure.seyidova@gmail.com>
2025-05-10 00:12:51 +0400

    header and button are done

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,7 +1,7 @@
 import type ButtonProps from "../../models/ButtonProps";
-
+import "./button.css";
 const Button = ({ name, style }: ButtonProps) => {
-  return <button className={`${style}`}>{name}</button>;
+  return <button className={`${style} button`}>{name}</button>;
 };
 
 export default Button;
--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,5 +1,47 @@
+import { ShoppingBag } from "lucide-react";
+import { NavLink } from "react-router-dom";
+import "./header.css";
+import logo from "../../assets/svg/gymfit logo.svg";
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <div className="header">
+      <div className="logo">
+        <img src={logo} alt="logo" />
+      </div>
+      <nav>
+        <ul>
+          <NavLink
+            className={({ isActive }) => (isActive ? "active" : "")}
+            to={"/"}
+          >
+            Home
+          </NavLink>
+          <NavLink
+            className={({ isActive }) => (isActive ? "active" : "")}
+            to={"/about"}
+          >
+            About
+          </NavLink>
+          <NavLink
+            className={({ isActive }) => (isActive ? "active" : "")}
+            to={"/class"}
+          >
+            Classes
+          </NavLink>
+          <NavLink
+            className={({ isActive }) => (isActive ? "active" : "")}
+            to={"/shop"}
+          >
+            Shop
+          </NavLink>
+        </ul>
+      </nav>
+      <div className="shoppingBag">
+        <ShoppingBag />
+        <span className="count">0</span>
+      </div>
+    </div>
+  );
 };
 
 export default Header;
